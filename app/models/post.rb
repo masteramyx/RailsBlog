1ed17303874fb5cfd9cf193c25c74cf7dd5ddbd7
Kyle Amyx <masteramyx@gmail.com>
2017-05-31 15:07:47 -0400

    Added define_method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,16 @@ class Post < ApplicationRecord
   validates :title, :presence => true
   #has_many is an association
   has_many :comments
+
+  def self.method_missing(name, *args, &block)
+    if name =~ /\Aquery_by_(.+)\z/
+      where($1 => args[0]).first
+    else
+      super
+    end
+  end
+
+  def self.respond_to_missing?(name, include_all=false)
+    name.to_s.start_with?("query_by_") || super
+  end
 end
